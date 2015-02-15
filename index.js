fdb09503c0edac6b24f3e1c7eb39b370f90d134d
iwatakeshi <iwatakeshi@gmail.com>
2015-02-14 19:28:09 -0700

    1.1.0
    Added node and browser support.
    Added support for custom attributes using a mustache like syntax.
    Removed functon 'id' so that you can pass the id and call the desired
    parser such as 'youtube' and 'vimeo'.
    Added html example using jquery.

--- index.js
+++ index.js
@@ -1,41 +1,164 @@
-var URL = require("url")
+/*jslint node: true, forin: true, white: true, newcap: true*/
+/*jslint browser:true */
+/*
+ * embed
+ * authors:
+ * Alan Shaw,
+ * Takeshi Iwana
+ * license : MIT
+ * Code heavily borrowed from Adam Draper
+ * https://github.com/adamwdraper
+ */
 
-function embed (url, opts) {
-  var id
+(function() {
+    'use strict'
+    var embed,
+        version = '1.1.0',
+        parsers = {
+            youtube: {
+                parse: function(url, callback) {
+                    if (url.host.indexOf("youtube.com") > -1) {
+                        url = url.query.v || url.query.replace(/v=/i, '');
+                    }
 
-  url = URL.parse(url, true)
+                    if (url.host == "youtu.be") {
+                        url = url.path.split("/")[1];
+                    }
+                    return callback(url, '<iframe src="https://www.youtube.com/embed/{{eid}}" frameborder="0" allowfullscreen class="{{class}}" id="{{id}}" width="{{width}}" height="{{height}}"></iframe>')
+                }
+            },
+            vimeo: {
+                parse: function(url, callback) {
+                    url = (url.host == "vimeo.com") ? url.path.split("/")[1] : null;
+                    return callback(url, '<iframe src="https://player.vimeo.com/video/{{eid}}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="{{class}}" id="{{id}}" width="{{width}}" height="{{height}}"></iframe>')
+                }
+            }
+        },
+        hasModule = (typeof module !== 'undefined' && module.exports);
 
-  id = detectYoutube(url)
-  if (id) return embed.youtube(id, opts)
+    /*
+        Helpers
+     */
+    // parseUri 1.2.2
+    // (c) Steven Levithan <stevenlevithan.com>
+    // http://blog.stevenlevithan.com/archives/parseuri
+    // MIT License
+    function Url(str, strict) {
+        Url.options.strictMode = strict ? false : true;
 
-  id = detectVimeo(url)
-  if (id) return embed.vimeo(id, opts)
-}
+        var o = Url.options,
+            m = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+            uri = {},
+            i = 14;
 
-function detectVimeo (url) {
-  return (url.hostname == "vimeo.com") ? url.pathname.split("/")[1] : null
-}
+        while (i--) {
+            uri[o.key[i]] = m[i] || "";
+        }
 
-function detectYoutube (url) {
-  if (url.hostname.indexOf("youtube.com") > -1) {
-    return url.query.v
-  }
+        uri[o.q.name] = {};
+        uri[o.key[12]].replace(o.q.parser, function($0, $1, $2) {
+            if ($1) uri[o.q.name][$1] = $2;
+        });
 
-  if (url.hostname == "youtu.be") {
-    return url.pathname.split("/")[1]
-  }
+        return uri;
+    };
 
-  return null
-}
+    Url.options = {
+        strictMode: false,
+        key: ["source", "protocol", "authority", "userInfo", "user", "password", "host", "port", "relative", "path", "directory", "file", "query", "anchor"],
+        q: {
+            name: "queryKey",
+            parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+        },
+        parser: {
+            strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+            loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+        }
+    };
 
-embed.vimeo = function (id, opts) {
-  // TODO: use opts to set query string, iframe attrs etc.
-  return '<iframe src="//player.vimeo.com/video/' + id + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
-}
+    //http://blakeembrey.com/articles/2014/01/wrapping-javascript-functions/
+    var __slice = Array.prototype.slice;
+    var wrap = function(fn, wrap) {
+        return function() {
+            return wrap.apply(this, [fn].concat(__slice.call(arguments)));
+        };
+    };
 
-embed.youtube = function (id, opts) {
-  // TODO: use opts to set query string, iframe attrs etc.
-  return '<iframe src="//www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>'
-}
+    /*
+        Constructor
+     */
+    function Embed(arg, opt) {
+        this.arg = arg;
+        this.url = Url(arg, true);
+        var options = {};
+        //setup options
+        options.class = opt ? (opt.class || '') : '';
+        options.id = opt ? (opt.id || '') : '';
+        options.width = opt ? (opt.width || '100%') : '100%';
+        options.height = opt ? (opt.height || '100%') : '100%';
 
-module.exports = embed
+        this.opt = options;
+
+        for (var parser in parsers) {
+            if (parsers.hasOwnProperty(parser)) {
+                var self = this;
+                self[parser] = wrap(parsers[parser], function(fn) {
+                    return fn.parse(this.url, function(id, iframe) {
+                        var script = iframe;
+                        //http://stackoverflow.com/a/15502875/1251031
+                        iframe.match(/{{\s*[\w\.]+\s*}}/g)
+                            .map(function(x) {
+                                if (x.match(/eid/)) {
+                                    if (typeof id === 'object' && id !== null) {
+
+                                        script = script.replace(new RegExp(x.toString(), "g"), id.source);
+                                    } else if (typeof id === 'string' && id !== null) {
+                                        script = script.replace(new RegExp(x.toString(), "g"), id);
+                                    } else if (id === null) {
+                                        script = script.replace(new RegExp(x.toString(), "g"), self.url.source);
+                                    }
+
+                                } else {
+                                    script = script.replace(new RegExp(x.toString(), "g"), self.opt[x.substring(2, x.length - 2)] || "");
+                                }
+                            });
+
+                        return script;
+                    });
+                });
+            }
+        }
+    }
+
+    /**
+     * Main function
+     */
+    embed = function(arg, opt) {
+        return new Embed(arg, opt)
+    };
+
+
+    /************************************
+          Exposing embed
+      ************************************/
+
+    // CommonJS module is defined
+    if (hasModule) {
+        module.exports = embed;
+    }
+
+    /*global ender:false */
+    if (typeof ender === 'undefined') {
+        // here, `this` means `window` in the browser, or `global` on the server
+        // add `embed` as a global object via a string identifier,
+        // for Closure Compiler 'advanced' mode
+        this.embed = embed;
+    }
+
+    /*global define:false */
+    if (typeof define === 'function' && define.amd) {
+        define([], function() {
+            return embed;
+        });
+    }
+}).call(this);
--- test.js
+++ test.js
@@ -1,32 +1,47 @@
-var test = require("tape")
-  , embed = require("./")
+var test = require("tape"),
+    embed = require("./index");
 
-test("convert vimeo.com url", function (t) {
-  t.plan(1)
-  var code = embed("http://vimeo.com/19339941")
-  t.equal(code, '<iframe src="//player.vimeo.com/video/19339941" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+test("convert vimeo.com url", function(t) {
+    t.plan(1)
+    var code = embed("http://vimeo.com/19339941").vimeo();
+    t.equal(code, '<iframe src="https://player.vimeo.com/video/19339941" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="" id="" width="100%" height="100%"></iframe>')
+
+    t.plan(2)
+    code = embed("http://vimeo.com/19339941", {
+        height: '100'
+    }).vimeo();
+
+    t.equal(code, '<iframe src="https://player.vimeo.com/video/19339941" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="" id="" width="100%" height="100"></iframe>')
 })
 
-test("convert youtube.com url", function (t) {
-  t.plan(1)
-  var code = embed("https://www.youtube.com/watch?v=twE64AuqE9A")
-  t.equal(code, '<iframe src="//www.youtube.com/embed/twE64AuqE9A" frameborder="0" allowfullscreen></iframe>')
+test("convert youtube.com url", function(t) {
+    t.plan(1)
+    var code = embed("https://www.youtube.com/watch?v=twE64AuqE9A").youtube();
+    t.equal(code, '<iframe src="https://www.youtube.com/embed/twE64AuqE9A" frameborder="0" allowfullscreen class="" id="" width="100%" height="100%"></iframe>')
+
+    t.plan(2)
+
+    code = embed("https://www.youtube.com/watch?v=twE64AuqE9A", {
+        height: '300',
+        width: '300'
+    }).youtube();
+    t.equal(code, '<iframe src="https://www.youtube.com/embed/twE64AuqE9A" frameborder="0" allowfullscreen class="" id="" width="300" height="300"></iframe>')
 })
 
-test("convert youtu.be url", function (t) {
-  t.plan(1)
-  var code = embed("http://youtu.be/9XeNNqeHVDw#aid=P-Do3JLm4A0")
-  t.equal(code, '<iframe src="//www.youtube.com/embed/9XeNNqeHVDw" frameborder="0" allowfullscreen></iframe>')
+test("convert youtu.be url", function(t) {
+    t.plan(1)
+    var code = embed("http://youtu.be/9XeNNqeHVDw#aid=P-Do3JLm4A0").youtube();
+    t.equal(code, '<iframe src="https://www.youtube.com/embed/9XeNNqeHVDw" frameborder="0" allowfullscreen class="" id="" width="100%" height="100%"></iframe>')
 })
 
-test("convert vimeo id", function (t) {
-  t.plan(1)
-  var code = embed.vimeo("19339941")
-  t.equal(code, '<iframe src="//player.vimeo.com/video/19339941" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+test("convert vimeo id", function(t) {
+    t.plan(1)
+    var code = embed("19339941").vimeo()
+    t.equal(code, '<iframe src="https://player.vimeo.com/video/19339941" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="" id="" width="100%" height="100%"></iframe>')
 })
 
-test("convert youtube id", function (t) {
-  t.plan(1)
-  var code = embed.youtube("9XeNNqeHVDw")
-  t.equal(code, '<iframe src="//www.youtube.com/embed/9XeNNqeHVDw" frameborder="0" allowfullscreen></iframe>')
+test("convert youtube id", function(t) {
+    t.plan(1)
+    var code = embed("9XeNNqeHVDw").youtube()
+    t.equal(code, '<iframe src="https://www.youtube.com/embed/9XeNNqeHVDw" frameborder="0" allowfullscreen class="" id="" width="100%" height="100%"></iframe>')
 })
